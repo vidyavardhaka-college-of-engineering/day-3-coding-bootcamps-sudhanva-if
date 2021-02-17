337a9b6ac52e8606218610db3c73aafa44de3f8a
sudhanvaif <79088359+sudhanva-if@users.noreply.github.com>
2021-02-17 12:10:32 +0000

    even or odd function is used

--- Problem_3.c
+++ Problem_3.c
@@ -1,13 +1,23 @@
 // Check whether an integer is odd or even
 
 #include <stdio.h>
-int main() {
-    int number;
+int main()
+ {
+    int n;
     printf("Enter an integer: ");
-    scanf("%d", &number);
+    scanf("%d", &n);
 
     // Write your code here
-    
+  
+     if(n%2==0)
+     {
+      printf("even");
+     }
+     else
+
+     {
+    printf("odd");
+    }
     
    
     return 0;
